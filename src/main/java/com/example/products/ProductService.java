838bd97dfd40fd855397339f700cb0c2e5f06cb5
Abdallah <hameurlain.abdallah@gmail.com>
2023-04-26 08:45:03 +0100

    fix: add correct responseStatus on error, fix adding elements when they're of status "broken", NPE, barcode, BigDecimal, Naming convention ( stick to english ), using LowerCase when needed.

--- src/main/java/com/example/products/ProductService.java
+++ src/main/java/com/example/products/ProductService.java
@@ -1,6 +1,8 @@
 package com.example.products;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,42 +11,45 @@ import lombok.Data;
 @Service
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    public Inventory getProductsData() {
-        Inventory inventory = new Inventory();
-        for(Product p: productRepository.findAll()) {
-            if (inventory.containsKey(p.getName())) {
-                InventoryItem productItem = inventory.get(p.getName());
-                if (!p.getState().equals("broken")) {
-                    productItem.setQty(productItem.getQty() + 1);
-                    productItem.setTotalPrice(productItem.getTotalPrice() + p.getPrice());
-                    productItem.productBarcodes += "," + p.getBarcode();
-                }
-            }
-            else {
-                inventory.put(p.getName(), new InventoryItem(p.getName().toLowerCase(), p.getPrice(), 1));
-            }
-        }
-        return inventory;
-    }
+	public Inventory getProductsData() {
+		Inventory inventory = new Inventory();
+		for (Product product : productRepository.findAll()) {
+			if (inventory.containsKey(product.getName().toLowerCase())) {
+				InventoryItem productItem = inventory.get(product.getName().toLowerCase());
+				if (!Objects.equals(product.getState(), "broken")) {
+					productItem.setQty(productItem.getQty() + 1);
+					productItem.setTotalPrice(productItem.getTotalPrice() + product.getPrice());
+					productItem.productBarcodes += "," + product.getBarcode();
+				}
+			} else {
+				if (!Objects.equals(product.getState(), "broken")) {
+					inventory.put(product.getName().toLowerCase(), new InventoryItem(product.getName().toLowerCase(),
+							product.getPrice(), 1, product.getBarcode()));
+				}
+			}
+		}
+		return inventory;
+	}
 
-    public static class Inventory extends HashMap<String, InventoryItem> { }
+	public static class Inventory extends HashMap<String, InventoryItem> {
+	}
 
-    @Data
-    public static class InventoryItem {
-        private final String pName;
-        private int qty;
-        private float totalPrice;
-        private String productBarcodes;
+	@Data
+	public static class InventoryItem {
+		private final String pName;
+		private int qty;
+		private float totalPrice;
+		private String productBarcodes;
 
-        public InventoryItem(String pName, float totalPrice, int qty) {
-            this.pName = pName;
-            this.totalPrice = totalPrice;
-            this.qty = qty;
-        }
+		public InventoryItem(String pName, float totalPrice, int qty, String productBarcodes) {
+			this.pName = pName;
+			this.totalPrice = totalPrice;
+			this.qty = qty;
+			this.productBarcodes = productBarcodes;
+		}
 
-        // tous les getters and setters sont implémentés
-    }
+	}
 }
